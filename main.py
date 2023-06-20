c970a50ff607e8203727263f9f7bc0769810e170
Daniel Ramirez <dan.ram0121@gmail.com>
2023-06-19 23:27:36 -0400

    intro comments and implement imports

---
+++ main.py
@@ -0,0 +1,11 @@
+# CAP 4630 Intro to AI
+# Project 2 - The Traveling Salesman Problem (TSP) using Genetic Algorithms (GA)
+# Contributors: John Kantlehner and Daniel Ramirez
+# Start Date: 06/19/2023
+# End Date: 06/21/2023
+
+# Import libraries
+import numpy as np
+import math
+import random
+import matplotlib.pyplot as plt
